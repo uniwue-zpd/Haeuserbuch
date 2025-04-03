f2106d416f2aff948260764be0e19ae2a73b7f73
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-04-03 10:26:52 +0200

    Initializing the backend instance

---
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/HaeuserbuchBackendApplication.java
@@ -0,0 +1,13 @@
+package de.uniwue.dachs.haeuserbuch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaeuserbuchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaeuserbuchBackendApplication.class, args);
+	}
+
+}
---
+++ haeuserbuch_backend/src/test/java/de/uniwue/dachs/haeuserbuch_backend/HaeuserbuchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.uniwue.dachs.haeuserbuch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaeuserbuchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
