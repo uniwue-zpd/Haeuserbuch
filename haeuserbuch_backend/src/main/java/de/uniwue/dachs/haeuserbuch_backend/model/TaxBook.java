42d9b4442cf629c465f402caa032ea74c423ddfc
Tomash Shtohryn <tomash.shtohryn@uni-wuerzburg.de>
2025-04-25 10:48:48 +0200

    Data Model. Add new entity TaxBook - The entity represents the Viertelbuch/Steuerbuch objects

---
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/model/TaxBook.java
@@ -0,0 +1,17 @@
+package de.uniwue.dachs.haeuserbuch_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "TAXBOOK")
+@Getter
+@Setter
+public class TaxBook extends BaseEntity {
+    private String title;
+    private String signature;
+}
+
+// TODO: Needs clarification
