01644064a50d00c01db3be583b545d36e8b17efe
Tomash Shtohryn <tomash.shtohryn@uni-wuerzburg.de>
2025-04-25 10:12:53 +0200

    Data Model. Add new entity Citizenship - The entity represents some entry in CitizenRegister (TownBook) - Each entry creates the relation to Person, TownBook and Place entities using ID as FK - Other fields are providing the possibility to add further details

---
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/model/Citizenship.java
@@ -0,0 +1,37 @@
+package de.uniwue.dachs.haeuserbuch_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "CITIZENSHIP")
+@Getter
+@Setter
+public class Citizenship extends BaseEntity {
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "person_id", nullable = false)
+    private Person person;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "townbook_id", nullable = false)
+    private TownBook townbook;
+
+    @ManyToOne
+    @JoinColumn(name = "place_id")
+    private Place place;
+
+    private Long number;
+
+    private String date;
+
+    private String entry_text;
+
+    private String addendum;
+}
+
+// TODO: Check whether a more precise representation of the dates is possible
