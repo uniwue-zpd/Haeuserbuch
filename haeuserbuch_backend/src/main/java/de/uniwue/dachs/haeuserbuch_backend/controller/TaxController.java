d46d67903c2453acc835ac514842426d9ad9ab98
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-05-13 15:19:52 +0200

    Data model. Implement an endpoint and the service layer for Tax entities

---
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/controller/TaxController.java
@@ -0,0 +1,48 @@
+package de.uniwue.dachs.haeuserbuch_backend.controller;
+
+import de.uniwue.dachs.haeuserbuch_backend.DTO.TaxDTO;
+import de.uniwue.dachs.haeuserbuch_backend.model.Tax;
+import de.uniwue.dachs.haeuserbuch_backend.service.TaxService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/taxes")
+public class TaxController {
+    private final TaxService taxService;
+
+    public TaxController(TaxService taxService) {
+        this.taxService = taxService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaxDTO>> getAllTaxes() {
+        List<TaxDTO> taxDTOS = taxService.getAllTaxes();
+        return ResponseEntity.ok(taxDTOS);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TaxDTO> getTaxById(@PathVariable Long id) {
+        return taxService.getTaxById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(404).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Tax> createTax(@RequestBody TaxDTO taxDTO) {
+        taxService.createTax(taxDTO);
+        return ResponseEntity.status(201).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTaxById(@PathVariable Long id) {
+        try {
+            taxService.deleteTax(id);
+            return ResponseEntity.status(204).build();
+        } catch (Exception e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
+}
---
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/service/TaxService.java
@@ -0,0 +1,135 @@
+package de.uniwue.dachs.haeuserbuch_backend.service;
+
+import de.uniwue.dachs.haeuserbuch_backend.DTO.BuildingDTO;
+import de.uniwue.dachs.haeuserbuch_backend.DTO.TaxDTO;
+import de.uniwue.dachs.haeuserbuch_backend.model.Building;
+import de.uniwue.dachs.haeuserbuch_backend.model.Person;
+import de.uniwue.dachs.haeuserbuch_backend.model.Tax;
+import de.uniwue.dachs.haeuserbuch_backend.model.TaxBook;
+import de.uniwue.dachs.haeuserbuch_backend.repository.BuildingRepository;
+import de.uniwue.dachs.haeuserbuch_backend.repository.PersonRepository;
+import de.uniwue.dachs.haeuserbuch_backend.repository.TaxBookRepository;
+import de.uniwue.dachs.haeuserbuch_backend.repository.TaxRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static de.uniwue.dachs.haeuserbuch_backend.utils.PostGIS.GeometryUtils.*;
+
+@Service
+public class TaxService {
+    private final BuildingRepository buildingRepository;
+    private final PersonRepository personRepository;
+    private final TaxBookRepository taxBookRepository;
+    private final TaxRepository taxRepository;
+
+    public TaxService(BuildingRepository buildingRepository,
+                      PersonRepository personRepository,
+                      TaxBookRepository taxBookRepository,
+                      TaxRepository taxRepository) {
+        this.buildingRepository = buildingRepository;
+        this.personRepository = personRepository;
+        this.taxBookRepository = taxBookRepository;
+        this.taxRepository = taxRepository;
+    }
+
+    // GET
+    public List<TaxDTO> getAllTaxes() {
+        List<Tax> taxes = taxRepository.findAll();
+        List<TaxDTO> taxDTOs = new ArrayList<>();
+        taxes.forEach(tax ->
+                taxDTOs.add(taxToDto(tax))
+        );
+        return taxDTOs;
+    }
+
+    // GET
+    public Optional<TaxDTO> getTaxById(Long id) {
+        return taxRepository.findById(id)
+                .map(this::taxToDto);
+    }
+
+    // POST
+    @Transactional
+    public void createTax(TaxDTO taxDTO) {
+        Tax tax = DtoToTax(taxDTO);
+        taxRepository.save(tax);
+    }
+
+    // DELETE
+    @Transactional
+    public void deleteTax(Long id) {
+        if (!taxRepository.existsById(id)) {
+            throw new IllegalArgumentException("Tax with id " + id + " does not exist");
+        }
+        taxRepository.deleteById(id);
+    }
+
+    // Helper methods
+    private Tax DtoToTax(TaxDTO taxDTO) {
+        Tax tax = new Tax();
+        tax.setTax_number(taxDTO.getTax_number());
+        tax.setPlan_number(taxDTO.getPlan_number());
+        tax.setEntry_text(taxDTO.getEntry_text());
+        tax.setBuilding(getOrSaveBuildingDTO(taxDTO.getBuilding()));
+        tax.setPerson(getOrSavePerson(taxDTO.getPerson()));
+        tax.setTaxbook(getOrSaveTaxBook(taxDTO.getTaxBook()));
+        return tax;
+    }
+
+    private TaxDTO taxToDto(Tax tax) {
+        TaxDTO taxDTO = new TaxDTO();
+        taxDTO.setId(tax.getId());
+        taxDTO.setTax_number(tax.getTax_number());
+        taxDTO.setPlan_number(tax.getPlan_number());
+        taxDTO.setEntry_text(tax.getEntry_text());
+        taxDTO.setBuilding(buildingToDto(tax.getBuilding()));
+        return taxDTO;
+    }
+
+    private BuildingDTO buildingToDto(Building building) {
+        BuildingDTO buildingDTO = new BuildingDTO();
+        buildingDTO.setId(building.getId());
+        buildingDTO.setName(building.getName());
+        buildingDTO.setAddress(building.getAddress());
+        buildingDTO.setDescription(building.getDescription());
+        buildingDTO.setQuarter(building.getQuarter());
+        buildingDTO.setDistrict(building.getDistrict());
+        buildingDTO.setCoordinates(convertPolygon(building.getCoordinates()));
+        return buildingDTO;
+    }
+
+    private Building getOrSaveBuildingDTO(BuildingDTO buildingDTO) {
+        if (buildingDTO == null) {
+            return null;
+        }
+        if (buildingDTO.getId() != null) {
+            return buildingRepository.findById(buildingDTO.getId()).orElse(null);
+        }
+        Building building = new Building();
+        building.setName(buildingDTO.getName());
+        building.setAddress(buildingDTO.getAddress());
+        building.setDescription(buildingDTO.getDescription());
+        building.setQuarter(buildingDTO.getQuarter());
+        building.setDistrict(buildingDTO.getDistrict());
+        building.setCoordinates(createPolygon(buildingDTO.getCoordinates()));
+        return buildingRepository.save(building);
+    }
+
+    private Person getOrSavePerson(Person person) {
+        if (person.getId() != null) {
+            return personRepository.findById(person.getId()).orElse(null);
+        }
+        return personRepository.save(person);
+    }
+
+    private TaxBook getOrSaveTaxBook(TaxBook taxBook) {
+        if (taxBook.getId() != null) {
+            return taxBookRepository.findById(taxBook.getId()).orElse(null);
+        }
+        return taxBookRepository.save(taxBook);
+    }
+}
