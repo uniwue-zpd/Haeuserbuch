2a0338525d2049571879ccdc3229fd3dfffeee5f
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-06-20 14:55:29 +0200

    Data model. Refactor the models "Ownership" and "Person" by changing the logic and adding additional fields

--- haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/model/Ownership.java
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/model/Ownership.java
@@ -11,26 +11,29 @@ import java.util.List;
 @Getter
 @Setter
 public class Ownership extends BaseEntity {
-    @ManyToMany
-    @JoinTable(
-            name = "OWNERSHIP_PERSON",
-            joinColumns = @JoinColumn(name = "ownership_id"),
-            inverseJoinColumns = @JoinColumn(name = "person_id")
-    )
-    private List<Person> persons;
+    private String type;
 
-    @ManyToMany
-    @JoinTable(
-            name = "OWNERSHIP_BUILDING",
-            joinColumns = @JoinColumn(name = "ownership_id"),
-            inverseJoinColumns = @JoinColumn(name = "building_id")
-    )
-    private List<Building> buildings;
+    private String date;
+
+    private Long price;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "owner_id", nullable = false)
+    private Person owner;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "seller_id")
+    private Person seller;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "building_id", nullable = false)
+    private Building building;
 
     @ManyToOne(optional = false)
     @JoinColumn(name = "source_id", nullable = false)
     private Source source;
 
+    @Column(length = 2000)
     private String entry_text;
 
     private String notes;
--- haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/model/Person.java
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/model/Person.java
@@ -19,6 +19,8 @@ public class Person extends BaseEntity{
 
     private String full_name;
 
+    private String sex;
+
     private String occupation;
 
     private Boolean is_citizen;
