775429505b7911de1379f284b299918cea4ecb5a
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-07-22 16:42:44 +0200

    Backend. Implement the REST Controller for the Ownership entity. Supported request types: - GET all entities - GET by entity by its id - POST create new entity - PUT update existing entity by its id - DELETE existing entity by its id

--- haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/controller/OwnershipController.java
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/controller/OwnershipController.java
@@ -1,8 +1,12 @@
 package de.uniwue.dachs.haeuserbuch_backend.controller;
 
+import de.uniwue.dachs.haeuserbuch_backend.DTO.OwnershipDTO;
 import de.uniwue.dachs.haeuserbuch_backend.service.OwnershipService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ownerships")
@@ -13,5 +17,46 @@ public class OwnershipController {
         this.ownershipService = ownershipService;
     }
 
-    // TODO: Implement all endpoints
+    @GetMapping
+    public ResponseEntity<List<OwnershipDTO>> getAllOwnerships() {
+        List<OwnershipDTO> ownerships = ownershipService.getAllOwnerships();
+        return  ResponseEntity.ok(ownerships);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OwnershipDTO> getOwnershipById(@PathVariable Long id) {
+        return ownershipService.getOwnershipById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.status(404).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createOwnership(@RequestBody OwnershipDTO ownershipDTO) {
+        try {
+            ownershipService.createOwnership(ownershipDTO);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).build();
+        }
+        return ResponseEntity.status(201).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateOwnership(@PathVariable Long id, @RequestBody OwnershipDTO ownershipDTO) {
+        try {
+            ownershipService.updateOwnership(id, ownershipDTO);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOwnership(@PathVariable Long id) {
+        try {
+            ownershipService.deleteOwnership(id);
+            return ResponseEntity.status(204).build();
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(404).build();
+        }
+    }
 }
