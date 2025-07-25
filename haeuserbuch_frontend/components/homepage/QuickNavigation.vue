5e0337b5705a0c766518cfaecaa08f7214c9a077
Tomash Shtohryn <tomash.shtohryn@uni-wuerzburg.de>
2025-07-25 15:50:23 +0200

    Frontend. Add quick nav overview to homepage

--- haeuserbuch_frontend/components/homepage/QuickNavigation.vue
+++ haeuserbuch_frontend/components/homepage/QuickNavigation.vue
@@ -5,19 +5,19 @@
 <template>
   <div class="flex flex-col gap-3 md:grid md:grid-cols-3">
     <NuxtLink to="/buildings" prefetch>
-      <Card class="hover:shadow-md">
+      <Card class="hover:shadow-md bg-[#F1F2F2]">
         <template #header>
           <div class="flex justify-center pt-5">
             <i class="pi pi-home text-5xl md:text-7xl"/>
           </div>
         </template>
         <template #title>
-          <h3 class="text-center text-xl text-black montserrat-headline font-bold">Häuser</h3>
+          <h3 class="text-center text-xl text-black montserrat-headline font-bold">Katasterplan</h3>
         </template>
       </Card>
     </NuxtLink>
     <NuxtLink to="/places" prefetch>
-      <Card class="hover:shadow-md">
+      <Card class="hover:shadow-md bg-[#F1F2F2]">
         <template #header>
           <div class="flex justify-center pt-5">
             <i class="pi pi-map-marker text-5xl md:text-7xl"/>
@@ -29,7 +29,7 @@
       </Card>
     </NuxtLink>
     <NuxtLink to="/persons" prefetch>
-      <Card class="hover:shadow-md">
+      <Card class="hover:shadow-md bg-[#F1F2F2]">
         <template #header>
           <div class="flex justify-center pt-5">
             <i class="pi pi-user text-5xl md:text-7xl"/>
@@ -40,7 +40,7 @@
         </template>
       </Card>
     </NuxtLink>
-    <Card class="hover:shadow-md">
+    <Card class="hover:shadow-md bg-[#F1F2F2]">
       <template #header>
         <div class="flex justify-center pt-5">
           <i class="pi pi-id-card text-5xl md:text-7xl"/>
@@ -50,7 +50,7 @@
         <h3 class="text-center text-xl text-black montserrat-headline font-bold">Bürgermatrikel</h3>
       </template>
     </Card>
-    <Card class="hover:shadow-md">
+    <Card class="hover:shadow-md bg-[#F1F2F2]">
       <template #header>
         <div class="flex justify-center pt-5">
           <i class="pi pi-book text-5xl md:text-7xl"/>
@@ -60,14 +60,14 @@
         <h3 class="text-center text-xl text-black montserrat-headline font-bold">Eigentumsverhältnisse</h3>
       </template>
     </Card>
-    <Card class="hover:shadow-md">
+    <Card class="hover:shadow-md bg-[#F1F2F2]">
       <template #header>
         <div class="flex justify-center pt-5">
           <i class="pi pi-building-columns text-5xl md:text-7xl"/>
         </div>
       </template>
       <template #title>
-        <h3 class="text-center text-xl text-black montserrat-headline font-bold">Steuer</h3>
+        <h3 class="text-center text-xl text-black montserrat-headline font-bold">Steuerbucheinträge</h3>
       </template>
     </Card>
   </div>
--- haeuserbuch_frontend/pages/index.vue
+++ haeuserbuch_frontend/pages/index.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-
+import QuickNavigation from "~/components/homepage/QuickNavigation.vue";
 </script>
 
 <template>
-
+  <div class="flex flex-col gap-4">
+    <div class="text-3xl font-bold montserrat-headline flex flex-row space-x-2">
+      <p>Willkommen im Häuserbuch</p>
+      <p class="text-black text-opacity-40"> ohne Häuser</p>
+    </div>
+    <div class="intro flex flex-col gap-2">
+      <h2 class="text-2xl  font-bold montserrat-headline">Einführung</h2>
+    </div>
+    <div class="navigation flex flex-col gap-2">
+      <h2 class="text-2xl font-bold montserrat-headline">Schnellzugriff</h2>
+      <QuickNavigation/>
+    </div>
+  </div>
 </template>
 
 <style scoped>
