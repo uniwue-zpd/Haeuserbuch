2521edbcead6639d0afe9c8bcf998ddc04c499df
Tomash Shtohryn <tomash.shtohryn@uni-wuerzburg.de>
2025-08-01 09:31:42 +0200

    Frontend. Add animations to the QuickNavigation component

--- haeuserbuch_frontend/components/homepage/QuickNavigation.vue
+++ haeuserbuch_frontend/components/homepage/QuickNavigation.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex flex-col gap-3 md:grid md:grid-cols-3">
     <NuxtLink to="/buildings" prefetch>
-      <Card class="hover:shadow-md bg-[#F1F2F2]">
+      <Card class="hover:shadow-md hover:scale-105 transition-transform duration-300 bg-[#F1F2F2]">
         <template #header>
           <div class="flex justify-center pt-5">
             <i class="pi pi-home text-5xl md:text-7xl"/>
@@ -17,7 +17,7 @@
       </Card>
     </NuxtLink>
     <NuxtLink to="/places" prefetch>
-      <Card class="hover:shadow-md bg-[#F1F2F2]">
+      <Card class="hover:shadow-md hover:scale-105 transition-transform duration-300 bg-[#F1F2F2]">
         <template #header>
           <div class="flex justify-center pt-5">
             <i class="pi pi-map-marker text-5xl md:text-7xl"/>
@@ -29,7 +29,7 @@
       </Card>
     </NuxtLink>
     <NuxtLink to="/persons" prefetch>
-      <Card class="hover:shadow-md bg-[#F1F2F2]">
+      <Card class="hover:shadow-md hover:scale-105 transition-transform duration-300 bg-[#F1F2F2]">
         <template #header>
           <div class="flex justify-center pt-5">
             <i class="pi pi-user text-5xl md:text-7xl"/>
@@ -40,7 +40,7 @@
         </template>
       </Card>
     </NuxtLink>
-    <Card class="hover:shadow-md bg-[#F1F2F2]">
+    <Card class="hover:shadow-md hover:scale-105 transition-transform duration-300 bg-[#F1F2F2]">
       <template #header>
         <div class="flex justify-center pt-5">
           <i class="pi pi-id-card text-5xl md:text-7xl"/>
@@ -50,7 +50,7 @@
         <h3 class="text-center text-xl text-black montserrat-headline font-bold">Bürgermatrikel</h3>
       </template>
     </Card>
-    <Card class="hover:shadow-md bg-[#F1F2F2]">
+    <Card class="hover:shadow-md hover:scale-105 transition-transform duration-300 bg-[#F1F2F2]">
       <template #header>
         <div class="flex justify-center pt-5">
           <i class="pi pi-book text-5xl md:text-7xl"/>
@@ -60,7 +60,7 @@
         <h3 class="text-center text-xl text-black montserrat-headline font-bold">Eigentumsverhältnisse</h3>
       </template>
     </Card>
-    <Card class="hover:shadow-md bg-[#F1F2F2]">
+    <Card class="hover:shadow-md hover:scale-105 transition-transform duration-300 bg-[#F1F2F2]">
       <template #header>
         <div class="flex justify-center pt-5">
           <i class="pi pi-building-columns text-5xl md:text-7xl"/>
