b948dbce0c7751a2a44c0d98e4553e0e77dd582d
Tomash Shtohryn <tomash.shtohryn@uni-wuerzburg.de>
2025-08-11 12:06:31 +0200

    Frontend. Enhance the form with new fields for geoJSON properties

--- haeuserbuch_frontend/components/forms/BuildingForm.vue
+++ haeuserbuch_frontend/components/forms/BuildingForm.vue
@@ -43,6 +43,119 @@ const submit = async (formData: Partial<BuildingInput>) => {
     <p class="roboto-plain">
       Füllen Sie bitte die untenstehenden Felder aus, um ein Objekt zu erstellen oder anzupassen
     </p>
+    <FormKit
+        type="form"
+        id="building_creation"
+        submit-label="Erstellen"
+        @submit="submit"
+        :actions="false"
+        :value="props.building ? props.building : {}"
+        :key="props.building?.id || 'create'"
+        #default="{ value }"
+    >
+      <div class="flex flex-col gap-2">
+        <FormKit type="hidden" name="type" value="Feature" />
+        <FormKit type="group" name="properties">
+          <div class="flex flex-col gap-2">
+            <FormKit
+                type="hidden"
+                name="type"
+                value="building"
+                contenteditable="false"
+            />
+            <div class="flex flex-row space-x-5">
+              <FormKit
+                  type="text"
+                  name="name"
+                  label="Name"
+                  prefix-icon="text"
+                  outer-class="max-w-full"
+              />
+              <FormKit
+                  type="number"
+                  number
+                  name="houseNumber"
+                  label="Hausnummer"
+                  prefix-icon="number"
+                  outer-class="max-w-full"
+              />
+            </div>
+            <div class="flex flex-row space-x-5">
+              <FormKit
+                  type="text"
+                  name="partType"
+                  label="Bauteil"
+                  prefix-icon="text"
+                  outer-class="max-w-full"
+              />
+              <FormKit
+                type="text"
+                name="specialStatus"
+                label="Sonderstatus"
+                prefix-icon="text"
+                outer-class="max-w-full"
+              />
+            </div>
+            <div class="flex flex-row space-x-5">
+              <FormKit
+                  type="text"
+                  name="querter"
+                  label="Viertel"
+                  prefix-icon="text"
+                  outer-class="max-w-full"
+              />
+              <FormKit
+                  type="text"
+                  name="district"
+                  label="Distrikt"
+                  prefix-icon="text"
+                  outer-class="max-w-full"
+              />
+              <FormKit
+                  type="text"
+                  name="districtHouseNumber"
+                  label="Distrikt & Hausnummer"
+                  prefix-icon="text"
+                  outer-class="max-w-full"
+              />
+            </div>
+            <FormKit
+                type="textarea"
+                name="internalNotes"
+                label="Notizen intern"
+                prefix-icon="list"
+                outer-class="max-w-full"
+            />
+            <FormKit
+                type="textarea"
+                name="generalNotes"
+                label="Notizen allgemein"
+                prefix-icon="list"
+                outer-class="max-w-full"
+            />
+          </div>
+        </FormKit>
+        <FormKit type="group" name="geometry">
+          <div class="flex flex-col gap-2">
+            <FormKit
+                type="hidden"
+                name="type"
+                label="Geometrietyp"
+            />
+            <FormKit
+                type="hidden"
+                name="coordinates"
+                label="Koordinaten"
+            />
+          </div>
+        </FormKit>
+      </div>
+      <div class="border-solid border-2 rounded-md p-5 bg-[#F1F2F5] mb-2">
+        <div class="font-mono">geoJSON-Preview</div>
+        <hr>
+        <pre wrap class="text-sm">{{ value }}</pre>
+      </div>
+    </FormKit>
   </div>
 </template>
 
