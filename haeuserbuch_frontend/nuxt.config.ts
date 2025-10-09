55cc1dd17e49f7a3b7b2450c7898bd70bc26796a
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-10-09 13:11:03 +0200

    Frontend and docker-compose setup. Switching to SSR API fetching (#14): - Enable using environmental variables in frontend container - Refactor the .env file - Refactor the nuxt.config.ts file - Implement a composable which allows using useFetch()

--- haeuserbuch_frontend/nuxt.config.ts
+++ haeuserbuch_frontend/nuxt.config.ts
@@ -4,14 +4,8 @@ import Aura from '@primeuix/themes/aura';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro: {
-    devProxy: {
-      '/api': {
-        target: process.env.API_BASE_URL || 'http://localhost:8080',
-        changeOrigin: true,
-        autoRewrite: true
-      }
-    }
+  runtimeConfig: {
+    apiBaseUrl: 'http://localhost:8080',
   },
   modules: [
       '@primevue/nuxt-module',
