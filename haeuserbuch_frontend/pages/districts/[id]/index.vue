61612e2dca4f0ae266e6cd7f92c1a6bb052ae098
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-10-15 09:57:31 +0200

    Frontend. Add datatables with related buildings to the pages of single districts and quarters

--- haeuserbuch_frontend/pages/districts/[id]/index.vue
+++ haeuserbuch_frontend/pages/districts/[id]/index.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import { onMounted } from "vue";
+import type { BuildingDTO } from "~/utils/types";
 
 const route = useRoute();
 const district_store = useDistrictStore();
+const building_store = useBuildingStore();
 const district_id = Number(route.params.id);
 const district_item = computed(() => district_store.current_district);
+const related_buildings = ref<BuildingDTO[]>([]);
 
 onMounted(async () => {
   await district_store.fetchDistrictById(district_id);
+  related_buildings.value = await building_store.filterBuildingsByPropertyId("district", district_id);
 });
 
 useHead(() => ({
@@ -18,12 +22,32 @@ useHead(() => ({
 <template>
   <Card v-show="district_item">
     <template #title>
-      <h1 class="text-3xl montserrat-headline font-bold text-black">{{ district_item?.name }}</h1>
+      <h1 class="text-3xl montserrat-headline font-bold text-black">Distrikt {{ district_item?.name }}</h1>
     </template>
-    <template>
-      <div v-show="district_item?.description">
-        <div class="text-xs roboto-plain font-bold">Beschreibung</div>
-        <div>{{ district_item?.description }}</div>
+    <template #content>
+      <div class="flex flex-col gap-2">
+        <div v-show="district_item?.description">
+          <div class="flex flex-col gap-2">
+            <div class="text-xs roboto-plain font-bold">Beschreibung</div>
+            <div>{{ district_item?.description }}</div>
+          </div>
+        </div>
+        <Divider/>
+        <div v-show="related_buildings.length > 0" class="flex flex-col gap-2">
+          <h2 class="text-lg montserrat-headline font-bold text-black">Zugeordnete Gebäude</h2>
+          <DataTable :value="related_buildings" paginator :rows="10" stripedRows>
+            <Column field="districtHouseNumber" header="Bezeichnung" :sortable="true">
+              <template #body="{ data }">
+                <NuxtLink
+                    :to="`/buildings/${data.id}`"
+                    class="roboto-plain font-bold text-black"
+                >
+                  {{ data.districtHouseNumber }}
+                </NuxtLink>
+              </template>
+            </Column>
+          </DataTable>
+        </div>
       </div>
     </template>
     <template #footer>
--- haeuserbuch_frontend/pages/quarters/[id]/index.vue
+++ haeuserbuch_frontend/pages/quarters/[id]/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import type {BuildingDTO} from "~/utils/types";
+
 const route = useRoute();
 const quarter_store = useQuarterStore();
+const building_store = useBuildingStore();
 const quarter_id = Number(route.params.id);
 const quarter_item = computed(() => quarter_store.current_quarter);
+const related_buildings = ref<BuildingDTO[]>([]);
 
 onMounted(async () => {
   await quarter_store.fetchQuarterById(quarter_id);
+  related_buildings.value = await building_store.filterBuildingsByPropertyId("quarter", quarter_id);
 });
 
 useHead(() => ({
@@ -18,10 +23,28 @@ useHead(() => ({
     <template #title>
       <h1 class="text-3xl montserrat-headline font-bold text-black">{{ quarter_item?.name }}</h1>
     </template>
-    <template>
-      <div v-show="quarter_item?.description">
-        <div class="text-xs roboto-plain font-bold">Beschreibung</div>
-        <div>{{ quarter_item?.description }}</div>
+    <template #content>
+      <div class="flex flex-col gap-2">
+        <div v-show="quarter_item?.description">
+          <div class="text-xs roboto-plain font-bold">Beschreibung</div>
+          <div>{{ quarter_item?.description }}</div>
+        </div>
+        <Divider/>
+        <div v-show="related_buildings.length > 0" class="flex flex-col gap-2">
+          <h2 class="text-lg montserrat-headline font-bold text-black">Zugeordnete Gebäude</h2>
+          <DataTable :value="related_buildings" paginator :rows="10" stripedRows>
+            <Column field="districtHouseNumber" header="Bezeichnung" :sortable="true">
+              <template #body="{ data }">
+                <NuxtLink
+                    :to="`/buildings/${data.id}`"
+                    class="roboto-plain font-bold text-black"
+                >
+                  {{ data.districtHouseNumber }}
+                </NuxtLink>
+              </template>
+            </Column>
+          </DataTable>
+        </div>
       </div>
     </template>
     <template #footer>
