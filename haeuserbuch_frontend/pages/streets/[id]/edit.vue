da52fcba23877fc30fc890837cf2588d1705740d
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-10-20 08:53:56 +0200

    Frontend. Create a template which allows editing existing streets

---
+++ haeuserbuch_frontend/pages/streets/[id]/edit.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import StreetForm from "~/components/forms/StreetForm.vue";
+
+const route = useRoute();
+const street_id = Number(route.params.id);
+const street_store = useStreetStore();
+const street_item = computed(() => street_store.current_street);
+
+onMounted(async () => {
+  await street_store.fetchStreetById(street_id);
+});
+
+useHead(() => ({
+  title: `Straße bearbeiten`
+}));
+</script>
+
+<template>
+  <StreetForm
+      header=""
+      action="edit"
+      :street="street_item ?? undefined"
+  />
+</template>
+
+<style scoped>
+
+</style>
