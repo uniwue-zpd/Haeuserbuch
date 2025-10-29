c44fc8ab4cf8555a6f1357c7b9ad257d1e544a9a
Tomash Shtohryn <tomashshtohryn@gmail.com>
2025-10-29 08:36:51 +0100

    Backend. Set a password auth for the tileserver

--- haeuserbuch_frontend/pages/index.vue
+++ haeuserbuch_frontend/pages/index.vue
@@ -12,9 +12,11 @@ useHead(() => ({
       <p>Willkommen im Häuserbuch</p>
       <p class="text-black text-opacity-40"> ohne Häuser</p>
     </div>
+    <!--
     <div class="intro flex flex-col gap-2">
       <h2 class="text-2xl  font-bold montserrat-headline">Einführung</h2>
     </div>
+    -->
     <div class="navigation flex flex-col gap-2">
       <h2 class="text-2xl font-bold montserrat-headline">Schnellzugriff</h2>
       <QuickNavigation/>
