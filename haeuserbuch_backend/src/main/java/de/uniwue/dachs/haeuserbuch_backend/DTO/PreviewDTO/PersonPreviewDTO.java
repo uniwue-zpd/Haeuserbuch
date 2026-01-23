8b8c572c47cda44db7ce8d37e14cb419667d3478
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-01-23 13:44:46 +0100

    Backend. Add missing preview DTO for the 'Person' entity in order to fix the problem causing a build error in e828749

---
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/DTO/PreviewDTO/PersonPreviewDTO.java
@@ -0,0 +1,12 @@
+package de.uniwue.dachs.haeuserbuch_backend.DTO.PreviewDTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PersonPreviewDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
