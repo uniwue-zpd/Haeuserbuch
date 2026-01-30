74f1316a7f66ae4536a84ce25f2740d90cc1b5e6
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-01-30 09:27:32 +0100

    Frontend. Replace the PrimeVue's Card template with custom layout in order to improve the readability of pages displaying information about citizens (#23)

--- haeuserbuch_frontend/pages/persons/[id]/index.vue
+++ haeuserbuch_frontend/pages/persons/[id]/index.vue
@@ -24,14 +24,12 @@ useHead(() => ({
 </script>
 
 <template>
-  <Card v-show="person_item">
-    <template #title>
-      <div class="flex flex-row justify-between">
-        <h1 class="text-3xl montserrat-headline font-bold text-black">{{ person_item?.fullName }}</h1>
-        <TaskBar :id="person_id" entity_type="persons"/>
-      </div>
-    </template>
-    <template #content>
+  <div class="flex flex-col gap-4 rounded-md shadow-md p-4">
+    <div class="flex flex-row justify-between">
+      <h1 class="text-3xl montserrat-headline font-bold text-black">{{ person_item?.fullName }}</h1>
+      <TaskBar :id="person_id" entity_type="persons"/>
+    </div>
+    <div class="p-4 rounded-md shadow-md bg-gray-100">
       <table class="text-black roboto-plain w-full table-auto">
         <tbody class="divide-y divide-gray-200">
         <tr v-show="person_item?.firstName">
@@ -72,10 +70,10 @@ useHead(() => ({
           <td class="px-6 py-4 whitespace-nowrap font-bold">Religion</td>
           <td class="px-6 py-4 whitespace-nowrap">{{ person_item?.confession }}</td>
         </tr>
-        <tr v-show="person_origin">
+        <tr v-show="person_origin?.originalText">
           <td class="px-6 py-4 whitespace-nowrap font-bold">Herkunft</td>
           <td class="px-6 py-4 whitespace-nowrap">
-            <div class="flex flex-col gap-1 rounded-md shadow-md p-2 bg-gray-100">
+            <div class="flex flex-col gap-1 rounded-md shadow-md p-2 bg-gray-200">
               <div v-if="person_origin?.originalText" class="flex flex-row space-x-3">
                 <span class="font-bold">Eingetragener Ort:</span>
                 <span>{{ person_origin.originalText }}</span>
@@ -86,7 +84,7 @@ useHead(() => ({
                   <div v-for="place in person_origin.places">
                     <NuxtLink
                         :to="`/places/${ place.id }`"
-                        class="p-1 bg-gray-200 rounded-md shadow-md hover:shadow-lg"
+                        class="p-1 bg-gray-300 rounded-md shadow-md hover:shadow-lg"
                     >
                       {{ place.realName }}
                     </NuxtLink>
@@ -102,29 +100,26 @@ useHead(() => ({
         </tr>
         </tbody>
       </table>
-    </template>
-    <template #footer>
-      <div class="flex flex-col gap-2">
-        <Panel header="Notizen" toggleable v-show="person_item?.generalNotes">
-          <template #header>
-            <p class="text-sm text-black roboto-plain font-bold">Notizen</p>
-          </template>
-          <p class="text-sm text-black roboto-plain">{{ person_item?.generalNotes }}</p>
-        </Panel>
-        <Divider/>
-        <div class="flex flex-col">
-          <div v-if="person_item?.createdDate" class="flex flex-row space-x-2 text-black roboto-plain">
-            <p class="font-bold">Erstellt am:</p>
-            <p>{{ new Date(person_item?.createdDate).toLocaleDateString() }}</p>
-          </div>
-          <div v-if="person_item?.lastModifiedDate" class="flex flex-row space-x-2 text-black roboto-plain">
-            <p class="font-bold">Stand:</p>
-            <p>{{ new Date(person_item?.lastModifiedDate).toLocaleDateString() }}</p>
-          </div>
+    </div>
+    <div class="flex flex-col gap-2 p-4 rounded-md shadow-md bg-gray-100">
+      <Panel header="Notizen" toggleable v-show="person_item?.generalNotes">
+        <template #header>
+          <p class="text-sm text-black roboto-plain font-bold">Notizen</p>
+        </template>
+        <p class="text-sm text-black roboto-plain">{{ person_item?.generalNotes }}</p>
+      </Panel>
+      <div class="flex flex-col">
+        <div v-if="person_item?.createdDate" class="flex flex-row space-x-2 text-black roboto-plain">
+          <p class="font-bold">Erstellt am:</p>
+          <p>{{ new Date(person_item?.createdDate).toLocaleDateString() }}</p>
+        </div>
+        <div v-if="person_item?.lastModifiedDate" class="flex flex-row space-x-2 text-black roboto-plain">
+          <p class="font-bold">Stand:</p>
+          <p>{{ new Date(person_item?.lastModifiedDate).toLocaleDateString() }}</p>
         </div>
       </div>
-    </template>
-  </Card>
+    </div>
+  </div>
 </template>
 
 <style scoped>
