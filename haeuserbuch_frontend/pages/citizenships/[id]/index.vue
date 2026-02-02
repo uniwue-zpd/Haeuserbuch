2d47f6009855842d9d8f0ea4d8b89cfdf510a1d5
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-02-02 15:46:10 +0100

    Frontend. Refactor the template showing metadata of single naturalization events in order to use updated data model design (#23)

--- haeuserbuch_frontend/pages/citizenships/[id]/index.vue
+++ haeuserbuch_frontend/pages/citizenships/[id]/index.vue
@@ -12,90 +12,85 @@ onMounted(async () => {
 });
 
 useHead(() => ({
-  title: citizenship_item.value ? `${citizenship_item.value.number} - Bürgermatrikel` : 'Nicht gefunden',
+  title: citizenship_item.value ? `${ citizenship_item.value.signature } - Bürgermatrikel` : 'Nicht gefunden',
 }));
 </script>
 
 <template>
-  <Card v-show="citizenship_item">
-    <template #title>
-      <div class="flex flex-row justify-between">
-        <h1 class="text-3xl montserrat-headline font-bold text-black">{{ citizenship_item?.signature }}  Nr.{{ citizenship_item?.number }}</h1>
-        <TaskBar :id="citizenship_id" entity_type="citizenships"/>
-      </div>
-    </template>
-    <template #content>
+  <div v-if="citizenship_item" class="flex flex-col gap-4 rounded-md shadow-md p-4">
+    <div class="flex flex-row justify-between">
+      <h1 class="text-3xl montserrat-headline font-bold text-black">{{ citizenship_item?.signature }}</h1>
+      <TaskBar :id="citizenship_id" entity_type="citizenships"/>
+    </div>
+    <div class="rounded-md shadow-md p-4 bg-gray-100">
       <table class="text-black roboto-plain w-full table-auto">
         <tbody v-if="citizenship_item" class="divide-y divide-gray-200">
-        <tr v-show="citizenship_item?.signature">
-          <td class="px-6 py-4 whitespace-nowrap font-bold">Signatur</td>
-          <td class="px-6 py-4 whitespace-nowrap">{{ citizenship_item?.signature }}</td>
+        <tr v-if="citizenship_item.signature">
+          <td class="py-4 whitespace-nowrap font-bold">Signatur</td>
+          <td class="py-4 whitespace-nowrap">{{ citizenship_item.signature }}</td>
         </tr>
-        <tr v-show="citizenship_item?.number">
-          <td class="px-6 py-4 whitespace-nowrap font-bold">Nummer</td>
-          <td class="px-6 py-4 whitespace-nowrap">{{ citizenship_item?.number }}</td>
+        <tr v-if="citizenship_item.refNumber">
+          <td class="py-4 whitespace-nowrap font-bold">Referenz Mayer-Erlach</td>
+          <td class="py-4 whitespace-nowrap">{{ citizenship_item.refNumber }}</td>
         </tr>
-        <tr v-show="citizenship_item?.date">
-          <td class="px-6 py-4 whitespace-nowrap font-bold">Datum</td>
-          <td class="px-6 py-4 whitespace-nowrap">{{ citizenship_item?.date }}</td>
+        <tr v-if="citizenship_item.date">
+          <td class="py-4 whitespace-nowrap font-bold">Datum</td>
+          <td class="py-4 whitespace-nowrap">{{ citizenship_item.date }}</td>
         </tr>
-        <tr v-if="citizenship_item.persons.length > 0">
-          <td class="px-6 py-4 whitespace-nowrap font-bold">Personen</td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <div class="flex flex-row space-x-2">
-              <div v-for="(person, index) in citizenship_item?.persons" :key="index">
-                <NuxtLink :to="`/persons/${person.id}`" class="rounded-md shadow-md hover:shadow-lg p-2 bg-[#F1F2F2]">
-                  {{ person.firstName }} {{ person.lastName }}
-                </NuxtLink>
-              </div>
-            </div>
-          </td>
-        </tr>
-        <tr v-show="citizenship_item?.place">
-          <td class="px-6 py-4 whitespace-nowrap font-bold">Ort</td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <NuxtLink :to="`/places/${citizenship_item?.place?.id}`" class="rounded-md shadow-md hover:shadow-lg p-2 bg-[#F1F2F2]">
-              {{ citizenship_item?.place?.realName }}
+        <tr v-if="citizenship_item.person">
+          <td class="py-4 whitespace-nowrap font-bold">Eingebürgerte Person</td>
+          <td class="py-4 whitespace-nowrap">
+            <NuxtLink :to="`/persons/${ citizenship_item.person.id }`" class="rounded-md shadow-md hover:shadow-lg p-2 bg-gray-200 font-medium">
+              {{ citizenship_item.person.firstName }} {{ citizenship_item.person.lastName }}
             </NuxtLink>
           </td>
         </tr>
-        <tr v-show="citizenship_item?.source">
-          <td class="px-6 py-4 whitespace-nowrap font-bold">Quelle</td>
-          <td class="px-6 py-4 whitespace-nowrap">
-            <NuxtLink :to="`/sources/${citizenship_item?.source.id}`" class="rounded-md shadow-md hover:shadow-lg p-2 bg-[#F1F2F2]">
-              {{ citizenship_item?.source.title }}
+        <tr v-if="citizenship_item.primarySource">
+          <td class="py-4 whitespace-nowrap font-bold">Primärquelle</td>
+          <td class="py-4 whitespace-nowrap">
+            <NuxtLink :to="`/sources/${ citizenship_item.primarySource.id }`" class="rounded-md shadow-md hover:shadow-lg p-2 bg-gray-200 font-medium">
+              {{ citizenship_item.primarySource.title }}
             </NuxtLink>
           </td>
         </tr>
-        <tr v-show="citizenship_item?.entryText">
-          <td class="px-6 py-4 whitespace-nowrap font-bold">Text</td>
-          <td class="px-6 py-4">{{ citizenship_item?.entryText }}</td>
+        <tr v-if="citizenship_item.secondarySource">
+          <td class="py-4 whitespace-nowrap font-bold">Sekundärquelle</td>
+          <td class="py-4 whitespace-nowrap">
+            <NuxtLink :to="`/sources/${ citizenship_item.secondarySource.id }`" class="rounded-md shadow-md hover:shadow-lg p-2 bg-gray-200 font-medium">
+              {{ citizenship_item.secondarySource.title }}
+            </NuxtLink>
+          </td>
+        </tr>
+        <tr v-if="citizenship_item.entryText">
+          <td class="py-4 whitespace-nowrap font-bold">Text des Eintrags</td>
+          <td class="py-4">{{ citizenship_item.entryText }}</td>
+        </tr>
+        <tr v-if="citizenship_item.addendum">
+          <td class="py-4 whitespace-nowrap font-bold">Nachtrag</td>
+          <td class="py-4">{{ citizenship_item.addendum }}</td>
         </tr>
         </tbody>
       </table>
-    </template>
-    <template #footer>
-      <div class="flex flex-col gap-2">
-        <Panel header="Notizen" toggleable v-show="citizenship_item?.generalNotes">
-          <template #header>
-            <p class="text-sm text-black roboto-plain font-bold">Notizen</p>
-          </template>
-          <p class="text-sm text-black roboto-plain">{{ citizenship_item?.generalNotes }}</p>
-        </Panel>
-        <Divider/>
-        <div class="flex flex-col">
-          <div v-if="citizenship_item?.createdDate" class="flex flex-row space-x-2 text-sm text-black roboto-plain">
-            <p>Erstellt am:</p>
-            <p>{{ new Date(citizenship_item?.createdDate).toLocaleDateString() }}</p>
-          </div>
-          <div v-if="citizenship_item?.lastModifiedDate" class="flex flex-row space-x-2 text-sm text-black roboto-plain">
-            <p>Stand:</p>
-            <p>{{ new Date(citizenship_item?.lastModifiedDate).toLocaleDateString() }}</p>
-          </div>
+    </div>
+    <div class="flex flex-col gap-2 p-4 rounded-md shadow-md bg-gray-100">
+      <Panel header="Notizen" toggleable v-show="citizenship_item?.generalNotes">
+        <template #header>
+          <p class="text-sm text-black roboto-plain font-bold">Notizen</p>
+        </template>
+        <p class="text-sm text-black roboto-plain">{{ citizenship_item?.generalNotes }}</p>
+      </Panel>
+      <div class="flex flex-col">
+        <div v-if="citizenship_item?.createdDate" class="flex flex-row space-x-2 text-base text-black roboto-plain">
+          <p class="font-bold">Erstellt am:</p>
+          <p>{{ new Date(citizenship_item?.createdDate).toLocaleDateString() }}</p>
+        </div>
+        <div v-if="citizenship_item?.lastModifiedDate" class="flex flex-row space-x-2 text-base text-black roboto-plain">
+          <p class="font-bold">Stand:</p>
+          <p>{{ new Date(citizenship_item?.lastModifiedDate).toLocaleDateString() }}</p>
         </div>
       </div>
-    </template>
-  </Card>
+    </div>
+  </div>
 </template>
 
 <style scoped>
--- haeuserbuch_frontend/utils/types.ts
+++ haeuserbuch_frontend/utils/types.ts
@@ -62,10 +62,10 @@ export interface BuildingNameDTO {
 /* Represents a naturalization event listed in the citizen register */
 export interface CitizenshipDTO extends Auditable {
     signature: string | null;
-    persons: PersonPreviewDTO[] | [];
-    source: SourceDTO;
-    place: PlaceDTO | null;
-    number: number | null;
+    person: PersonPreviewDTO | null;
+    primarySource: SourceDTO;
+    secondarySource: SourceDTO | null;
+    refNumber: number | null;
     date: string | null;
     entryText: string | null;
     addendum: string | null;
