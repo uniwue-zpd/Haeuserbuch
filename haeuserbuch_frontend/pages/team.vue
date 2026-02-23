b454ba5d37d25293ed7578950bf2ee91ee4bbf7f
Tomash Shtohryn <98087953+tomashshtohryn@users.noreply.github.com>
2026-02-23 13:20:34 +0100

    Static pages/team page (#25)
    * Frontend. Create a page of the project team on the route "/team" (#12)
    
    * Frontend. Add sections for e-mail-addresses and increase the gap between description areas (#12)
    
    * Frontend. Decrease the size of the "visit cards" by switching to the grid systems with 2 columns per row. The content remain flexbox on smaller screens (#12)
    
    * Frontend. Refactor team page layout and content (#12)
    
    * Frontend. Update team page (#12)
    
    * Frontend. Add link to team page in footer
    
    * Frontend. Add CV of remaining team members

---
+++ haeuserbuch_frontend/pages/team.vue
@@ -0,0 +1,233 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-3xl font-bold montserrat-headline pb-2">Das Team</div>
+  <div class="flex flex-col gap-4 w-[90%] mx-auto">
+    <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+      <div class="flex flex-col text-black montserrat-headline">
+        <h1 class="text-2xl text-black font-bold">Dr. Christian Naser</h1>
+        <h2 class="text-xl italic font-medium">Projektleitung</h2>
+      </div>
+      <Divider>
+        <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+      </Divider>
+      <div class="roboto-plain text-black text-justify">
+        Der akademische Direktor Christian Naser promovierte 1993 an der Universität Würzburg
+        mit der Arbeit „Der geistliche Streit. Synoptischer Abdruck der Fassungen A, C, B und
+        D. Kommentar und Motivgeschichte“. Seit 1987 arbeitet er als wissenschaftlicher Mitarbeiter
+        am Institut für dt. Philologie, zunächst in der Sprachwissenschaft, dann in der Mediävistik
+        und in der Neueren Abteilung. Sein Zuständigkeitsbereich sind die EDV-Belange in philologisch
+        ausgerichteten Forschungsprojekten. So übernahm er 1987 die EDV-Betreuung von den Würzburger
+        und Eichstätter Projekten des Sonderforschungsbereichs 226 (= Wissensorganisierende u.
+        wissensvermittelnde Literatur im Mittelalter). Ab 1994 betreute er die fünf Einzelprojekte
+        der interdisziplinären DFG-geförderten Forschergruppe „Das Bild des Krieges im Wandel vom
+        späten Mittelalter zur frühen Neuzeit“. Seit 2005 betreut Christian Naser das von der DFG
+        geförderte Langzeitprojekt der historisch-kritischen Jean- Paul-Ausgabe und seit 2022 im
+        Anschluss das Projekt der Bayerischen Akademie der Wissenschaften „Sprachgitter digital:
+        Die historisch-kritische Jean-Paul-Ausgabe“. Darüber hinaus leitet er ab Januar 2025 in
+        Kooperation mit dem Staatsarchiv Würzburg das DFG-Projekt „Würzburg: Häuserbuch ohne
+        Häuser Die sozialgeschichtliche und topographische Rekonstruktion einer untergegangenen
+        Stadt durch Auswertung und Zusammenführung verschiedenartiger Archivaliengattungen des
+        17. bis 19. Jahrhunderts“.
+      </div>
+      <Divider>
+        <div class="px-2 roboto-plain font-bold text-lg text-black">Kontakt</div>
+      </Divider>
+      <div class="flex flex-row space-x-3 roboto-plain">
+        <p class="font-bold">E-Mail:</p>
+        <NuxtLink to="mailto:christian.naser@uni-wuerzburg.de" target="_blank" class="text-blue-950">christian.naser@uni-wuerzburg.de</NuxtLink>
+      </div>
+    </div>
+    <Divider>
+      <div class="px-2 roboto-plain font-bold text-xl text-black">Mitarbeitende</div>
+    </Divider>
+    <div class="flex flex-col md:grid md:grid-cols-2 gap-4">
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <h1 class="text-2xl text-black font-bold">Dr. Alexander Wolz</h1>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+        </Divider>
+        <div class="roboto-plain text-black text-justify">
+          Dr. Alexander Wolz studierte Geschichte und Volkskunde an der Julius-Maximilians-Universität
+          in Würzburg. Im Jahr 2012 wurde er mit einer Arbeit über die Rheinlandkrise 1936 promoviert.
+          Nach einer kurzen Tätigkeit beim Stadtarchiv Lohr a. Main besuchte er von 2014 bis 2016 die
+          Bayerische Archivschule. Von 2016 bis 2021 leitete er das Staatsarchiv Coburg. Seit 2021
+          ist er der Leiter des Staatsarchivs Würzburg/Kitzingen. Wolz forscht zur fränkischen
+          Landesgeschichte, insbesondere mit den Schwerpunkten im 19. und 20. Jahrhundert.
+        </div>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Kontakt</div>
+        </Divider>
+        <div class="flex flex-row space-x-3 roboto-plain">
+          <p class="font-bold">E-Mail:</p>
+          <NuxtLink to="mailto:alexander.wolz@stakt.bayern.de" target="_blank" class="text-blue-950">alexander.wolz@stakt.bayern.de</NuxtLink>
+        </div>
+        <div class="flex flex-row space-x-3 roboto-plain">
+          <p class="font-bold">Dienststelle:</p>
+          <p class="text-black">Staatsarchiv Kitzingen, Deuster-Park 1, 97318 Kitzingen</p>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <h1 class="text-2xl text-black font-bold">Hans-Wolfgang Bergerhausen</h1>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+        </Divider>
+        <div class="roboto-plain text-black text-justify">
+          Hans-Wolfgang Bergerhausen lehrte seit der Habilitation 1999 als Privatdozent
+          und als außerplanmäßiger Professor bis 2018 Neuere Geschichte an der Universität
+          Würzburg. In seinen Arbeiten verknüpft er frühneuzeitliche Reichsgeschichte mit
+          vergleichender Landes- und Stadtgeschichte sowie der Konfessionalisierungsforschung.
+          Aus seinen intensiven archivalischen Recherchen zur Geschichte Würzburgs in der
+          Frühen Neuzeit ging 2014 eine umfangreiche Edition „Quellen zur Geschichte des
+          Bürgerspitals Würzburg 1500 – 1650“ hervor. Ihr folgten weitere ganz aus den Quellen
+          geschöpfte Arbeiten zum Fürsorgewesen in Würzburg und 2021 eine Biographie des Würzburger
+          Bürgermeisters und Spitalpflegers Paulus von Worms (+ 1579). Zum Reformationsjubiläum
+          verfasste er eine Studie über „Protestantisches Leben in Würzburg während des 16.
+          Jahrhunderts“. Ferner wirkte er an der Edition der „Würzburger Ratsprotokolle der
+          Riemenschneiderzeit“ mit, deren erster Band 2020 erschienen ist.
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <div class="flex flex-col text-black montserrat-headline">
+          <h1 class="text-2xl text-black font-bold">Julia Merz, M.A.</h1>
+          <h2 class="text-xl italic font-medium">Verarbeitung geospatialer Daten</h2>
+        </div>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+        </Divider>
+        <div class="flex flex-col gap-1 roboto-plain text-black text-justify">
+          <p>
+            Julia Merz, M.A., ist seit Januar 2025 wissenschaftliche Mitarbeiterin am Institut
+            für Deutsche Philologie der Julius-Maximilians-Universität Würzburg im von der
+            Deutschen Forschungsgemeinschaft (DFG) geförderten Würzburger Häuserprojekt unter
+            der Leitung von Dr. Christian Naser. Das Projekt verfolgt die Rekonstruktion der
+            historischen Sozialtopographie Würzburgs sowie die Geschichte ihrer Häuser und
+            Bewohnerinnen und Bewohner auf Grundlage bislang kaum erschlossener archivalischer
+            Quellen.
+          </p>
+          <p class="indent-4">
+            Nach ihrem Studium der Archäologie des Mittelalters und der Neuzeit, Bau- und
+            Kunstdenkmalpflege sowie Restaurierungswissenschaften an der Otto-Friedrich-Universität
+            Bamberg liegen ihre Schwerpunkte auf denkmalpflegerischen Erhebungen, kommunalen
+            Denkmalkonzepten und historischen Ortsanalysen. Ein besonderer Fokus ihrer Arbeit
+            liegt auf GIS-basierter Kartierung, 3D-Modellierung und Datenmodellierung im
+            kulturellen Kontext.
+          </p>
+          <p class="indent-4">
+            Im Würzburger Häuserprojekt ist Julia Merz verantwortlich für die räumliche
+            Erfassung, georeferenzierte Auswertung und denkmalpflegerische Kontextualisierung
+            der historischen Stadtstruktur. Ihre langjährige freiberufliche Erfahrung im
+            Bereich der städtebaulichen Denkmalpflege fließt dabei maßgeblich in ihre Arbeit ein.
+          </p>
+        </div>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Kontakt</div>
+        </Divider>
+        <div class="flex flex-row space-x-3 roboto-plain">
+          <p class="font-bold">E-Mail:</p>
+          <NuxtLink to="mailto:julia.merz@uni-wuerzburg.de" target="_blank" class="text-blue-950">julia.merz@uni-wuerzburg.de</NuxtLink>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <h1 class="text-2xl text-black font-bold">Dr. Renate Schindler</h1>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+        </Divider>
+        <div class="roboto-plain text-black text-justify">
+          Studium der Geschichte in Marburg und Bonn, dort Magister Artium
+          und Promotion an der Rheinischen Friedrich-Wilhelms-Universität.
+          Zweites Staatsexamen für den Archivdienst. Tätigkeit als Archivarin
+          in Koblenz, Wertheim und Würzburg.
+        </div>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Kontakt</div>
+        </Divider>
+        <div class="flex flex-row space-x-3 roboto-plain">
+          <p class="font-bold">ORCID:</p>
+          <NuxtLink to="https://orcid.org/0009-0000-2430-5616" target="_blank" class="text-blue-950">0009-0000-2430-5616</NuxtLink>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <h1 class="text-2xl text-black font-bold">Dr. Stefan Moser</h1>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+        </Divider>
+        <div class="roboto-plain text-black text-justify">
+          Stephan Moser studierte Germanistik und Geschichte
+          in Augsburg, Regensburg und Würzburg. 1987 bis 2024 arbeitete
+          er am Würzburger Institut für deutsche Philologie in den
+          Bereichen Sprachwissenschaft und Computerphilologie. 1994
+          promovierte er in der Deutschen Sprachwissenschaft mit einer
+          Arbeit zur Herausbildung der deutschen Fachsprachen im
+          Spätmittelalter.
+        </div>
+      </div>
+    </div>
+    <Divider>
+      <div class="px-2 roboto-plain font-bold text-xl text-black">Technische Umsetzung</div>
+    </Divider>
+    <div class="flex flex-col md:grid md:grid-cols-2 gap-4">
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <div class="flex flex-col text-black montserrat-headline">
+          <h1 class="text-2xl text-black font-bold">Tomash Shtohryn, M.A.</h1>
+          <h2 class="text-xl italic font-medium">Full-Stack-Entwicklung</h2>
+        </div>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+        </Divider>
+        <div class="roboto-plain text-black text-justify">
+          Tomash Shtohryn hat sein Studium in Fächern Political and Social Studies und Digital
+          Humanities als Nebenfach absolviert. Während des Masterstudiums machte er die Digital
+          Humanities zu seinem Hauptfach und ist aktuell wissenschaftlicher Mitarbeiter am Zentrum
+          für Philologie und Digitalität an der Universität Würzburg. Hier ist er an mehreren
+          Forschungsprojekten beteiligt, deren Schwerpunkt auf der Erstellung und Kuratierung von
+          Digitalen Editionen liegt. Auch die Entwicklung von vollwertigen interaktiven Ressourcen
+          auf Basis einer Datenbank macht einen wesentlichen Bestandteil seiner Tätigkeit aus.
+        </div>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Kontakt</div>
+        </Divider>
+        <div class="flex flex-row space-x-3 roboto-plain">
+          <p class="font-bold">E-Mail:</p>
+          <NuxtLink to="mailto:tomash.shtohryn@uni-wuerzburg.de" target="_blank" class="text-blue-950">tomash.shtohryn@uni-wuerzburg.de</NuxtLink>
+        </div>
+        <div class="flex flex-row space-x-3 roboto-plain">
+          <p class="font-bold">ORCID:</p>
+          <NuxtLink to="https://orcid.org/0009-0000-4597-603X" target="_blank" class="text-blue-950">0009-0000-4597-603X</NuxtLink>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <h1 class="text-2xl text-black font-bold">Florian Langhanki, M.A.</h1>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Kontakt</div>
+        </Divider>
+        <div class="flex flex-row space-x-3 roboto-plain">
+          <p class="font-bold">E-Mail:</p>
+          <NuxtLink to="mailto:florian.langhanki@uni-wuerzburg.de" target="_blank" class="text-blue-950">florian.langhanki@uni-wuerzburg.de</NuxtLink>
+        </div>
+      </div>
+      <div class="flex flex-col gap-2 p-5 rounded-xl shadow-lg border-2 border-[#F1F2F2]">
+        <div class="flex flex-col text-black montserrat-headline">
+          <h1 class="text-2xl text-black font-bold">Natália Ratulovská, B.A.</h1>
+          <h2 class="text-xl italic font-medium">Datentransformation und -verarbeitung</h2>
+        </div>
+        <Divider>
+          <div class="px-2 roboto-plain font-bold text-lg text-black">Vita</div>
+        </Divider>
+        <div class="roboto-plain text-black text-justify">
+          Natália Ratulovská absolvierte ihr Bachelorstudium an der LMU München, wobei
+          sie Germanistik als Hauptfach und Digital Humanities als Nebenfach belegte.
+          Gegenwärtig studiert sie im Masterstudiengang Digital Humanities an der JMU Würzburg.
+          Parallel zum Studium ist sie als Studentische Hilfskraft am Zentrum für Philologie
+          und Digitalität der JMU tätig.
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
