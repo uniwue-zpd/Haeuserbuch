b72bd3e09fa3d3c4ff0a31c43846843913572dce
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-03-30 10:09:13 +0200

    Backend. Add 'Weapon' entity and a DTO representing its instances (#30)

---
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/DTO/WeaponDTO.java
@@ -0,0 +1,12 @@
+package de.uniwue.dachs.haeuserbuch_backend.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WeaponDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
