39ee1e102ffa61ae0153dae688bfdd6362d04657
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-03-31 10:02:14 +0200

    Frontend. Enhance 'PersonDTO' interface by adding weapon-related interfaces for improved armament handling (#32)

--- haeuserbuch_frontend/utils/types.ts
+++ haeuserbuch_frontend/utils/types.ts
@@ -97,8 +97,10 @@ export interface Ownership extends Auditable {
     entryText: string | null;
 }
 
-/* DTO projection of a {@link Person} object */
-export interface PersonDTO extends Auditable{
+/**
+ * Represents a DTO of a person object mentioned in the sources.
+ */
+export interface PersonDTO extends Auditable {
     firstName: string | null;
     lastName: string | null;
     fullName: string | null;
@@ -116,6 +118,7 @@ export interface PersonDTO extends Auditable{
         originalText: string | null;
         certainty: OriginCertainty | null;
     };
+    weapons: WeaponryDTO[] | [];
 }
 
 export enum OriginCertainty {
@@ -216,4 +219,31 @@ export interface OccupationDTO {
     id: number | null;
     name: string | null;
     description: string | null;
+}
+
+/**
+ * Represents a weapon type, e.g. "Schwert" etc.
+ */
+export interface Weapon extends Auditable {
+    name: string | null;
+    description: string | null;
+}
+
+/**
+ * Represents a DTO of a weapon.
+ */
+export interface WeaponDTO {
+    id: number | null;
+    name: string | null;
+    description: string | null;
+}
+
+/**
+ * Represents a weapon mentioned in the sources.
+ * It contains the `ID`, `originalText` and a reference to the standardized {@link WeaponDTO} object.
+ */
+export interface WeaponryDTO {
+    id: number | null;
+    weapon: WeaponDTO | null;
+    originalText: string | null;
 }
