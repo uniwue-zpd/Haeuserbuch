8499284ccb57187011609fb0191d07a6b271b0e3
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-03-31 11:23:33 +0200

    Frontend. Integrate weapon selection in 'PersonForm' and initialize weapon store on session start (#32)

--- haeuserbuch_frontend/plugins/init_store.ts
+++ haeuserbuch_frontend/plugins/init_store.ts
@@ -12,6 +12,7 @@ export default defineNuxtPlugin(async (nuxtApp) => {
     const quarterStore = useQuarterStore();
     const citizenshipStore = useCitizenshipStore();
     const occupationStore = useOccupationStore();
+    const weaponStore = useWeaponStore();
 
     try {
         await Promise.all([
@@ -24,7 +25,8 @@ export default defineNuxtPlugin(async (nuxtApp) => {
             streetStore.fetchStreets(),
             quarterStore.fetchQuarters(),
             citizenshipStore.fetchCitizenships(),
-            occupationStore.fetchOccupations()
+            occupationStore.fetchOccupations(),
+            weaponStore.fetchWeapons()
         ]);
     } catch (error) {
         console.error("Error initializing stores:", error);
