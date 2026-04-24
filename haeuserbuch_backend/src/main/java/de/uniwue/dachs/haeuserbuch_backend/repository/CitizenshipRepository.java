9e1423a224bb2d79dadcfc965fc21a83e54a934b
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-04-24 13:56:06 +0200

    Backend. Enhance CitizenshipRepository to include entity graph for findAll method

--- haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/repository/CitizenshipRepository.java
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/repository/CitizenshipRepository.java
@@ -1,8 +1,21 @@
 package de.uniwue.dachs.haeuserbuch_backend.repository;
 
 import de.uniwue.dachs.haeuserbuch_backend.model.Citizenship;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CitizenshipRepository extends JpaRepository<Citizenship, Long> {}
+public interface CitizenshipRepository extends JpaRepository<Citizenship, Long> {
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = {
+            "person",
+            "primarySource",
+            "secondarySource"
+    })
+    List<Citizenship> findAll();
+}
