9b023758c69f4bcb57473dea1eee9a1f482547f5
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-04-27 09:28:45 +0200

    Backend. Enhance 'PersonRepository' to include entity graph for findAll method

--- haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/repository/PersonRepository.java
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/repository/PersonRepository.java
@@ -1,9 +1,23 @@
 package de.uniwue.dachs.haeuserbuch_backend.repository;
 
 import de.uniwue.dachs.haeuserbuch_backend.model.Person;
+import lombok.NonNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PersonRepository extends JpaRepository<Person, Long>, JpaSpecificationExecutor<Person> {}
+public interface PersonRepository extends JpaRepository<Person, Long>, JpaSpecificationExecutor<Person> {
+    @Override
+    @NonNull
+    @EntityGraph(attributePaths = {
+            "associatedBuilding",
+            "job.jobCategory",
+            "religion.religionCategory",
+            "altNames"
+    })
+    List<Person> findAll();
+}
