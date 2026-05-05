caa99009acc565754420287854b1d63951b98170
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-05-05 14:17:59 +0200

    Backend. Implement an endpoint for searching for weapons (#40)

--- haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/repository/WeaponRepository.java
+++ haeuserbuch_backend/src/main/java/de/uniwue/dachs/haeuserbuch_backend/repository/WeaponRepository.java
@@ -2,8 +2,18 @@ package de.uniwue.dachs.haeuserbuch_backend.repository;
 
 import de.uniwue.dachs.haeuserbuch_backend.model.Weapon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface WeaponRepository extends JpaRepository<Weapon, Long> {
+    @Query("""
+        SELECT DISTINCT w FROM Weapon w
+        WHERE LOWER(w.name) LIKE LOWER(CONCAT('%', :query, '%'))
+        ORDER BY w.name ASC
+    """)
+    List<Weapon> searchWeapons(@Param("query") String query);
 }
