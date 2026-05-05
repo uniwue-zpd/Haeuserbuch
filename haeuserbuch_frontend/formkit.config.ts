379403b1e26856270f6d57d7209ab387df10c6bf
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-05-05 09:30:11 +0200

    Frontend. Register custom input components for person and source searching in FormKit configuration (#40)

--- haeuserbuch_frontend/formkit.config.ts
+++ haeuserbuch_frontend/formkit.config.ts
@@ -1,10 +1,16 @@
-import { defaultConfig } from '@formkit/vue'
+import { defaultConfig, createInput } from '@formkit/vue'
 import { rootClasses } from './formkit.theme'
+import PersonSearch from "~/components/forms/custom_inputs/PersonSearch.vue";
+import SourceSearch from "~/components/forms/custom_inputs/SourceSearch.vue";
 
 const formkitConfig = defaultConfig({
     config: {
         rootClasses,
     },
+    inputs: {
+        personAutocomplete: createInput(PersonSearch, { props: ['isMultiple'] }),
+        sourceAutocomplete: createInput(SourceSearch)
+    }
 });
 
 export default formkitConfig;
