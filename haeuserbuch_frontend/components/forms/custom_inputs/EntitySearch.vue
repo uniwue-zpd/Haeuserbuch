603d7dd08572643c1b5a9c37b135ff16001c62e0
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-05-06 10:26:05 +0200

    Frontend. Add EntitySearch component for dynamic autocomplete functionality (#40)

---
+++ haeuserbuch_frontend/components/forms/custom_inputs/EntitySearch.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+const props = defineProps({
+  context: Object,
+});
+
+const loading = ref(false);
+
+type EntityTypes = 'building' | 'job' | 'person' | 'place' | 'religion' | 'source' | 'weapon';
+
+const entityType: EntityTypes = props.context?.entityType;
+const isMultiple: boolean = props.context?.isMultiple;
+const optionLabel: string = props.context?.optionLabel;
+
+const suggestions = ref<any[]>([]);
+
+const buildingStore = useBuildingStore();
+const jobStore = useJobStore();
+const personStore = usePersonStore();
+const placeStore = usePlaceStore();
+const religionStore = useReligionStore();
+const sourceStore = useSourceStore();
+const weaponStore = useWeaponStore();
+
+const debouncedSearch = debounce(async (query: string) => {
+  loading.value = true;
+  switch (entityType) {
+    case 'building':
+      suggestions.value = await buildingStore.searchBuildings(query);
+      break;
+    case 'job':
+      suggestions.value = await jobStore.searchJobs(query);
+      break;
+    case 'person':
+      suggestions.value = await personStore.searchPeople(query);
+      break;
+    case 'place':
+      suggestions.value = await placeStore.searchPlaces(query);
+      break;
+    case 'religion':
+      suggestions.value = await religionStore.searchReligions(query);
+      break;
+    case 'source':
+      suggestions.value = await sourceStore.searchSources(query);
+      break;
+    case 'weapon':
+      suggestions.value = await weaponStore.searchWeapons(query);
+      break;
+  }
+  loading.value = false;
+}, 300);
+
+const onComplete = (event: any) => {
+  debouncedSearch(event.query)
+}
+
+const value = computed({
+  get: () => props.context?.value ?? null,
+  set: (val) => props.context?.node.input(val),
+});
+</script>
+
+<template>
+  <AutoComplete
+      v-model="value"
+      :suggestions="suggestions"
+      :loading="loading"
+      @complete="onComplete"
+      @clear="isMultiple ? props.context?.node.input([]) : props.context?.node.input(null)"
+      :optionLabel="optionLabel"
+      class="min-w-full"
+      :dropdown="!isMultiple"
+      showClear
+      :multiple="isMultiple"
+  />
+</template>
+
+<style scoped>
+
+</style>
--- haeuserbuch_frontend/formkit.config.ts
+++ haeuserbuch_frontend/formkit.config.ts
@@ -1,25 +1,13 @@
 import { defaultConfig, createInput } from '@formkit/vue'
 import { rootClasses } from './formkit.theme'
-import PersonSearch from "~/components/forms/custom_inputs/PersonSearch.vue";
-import SourceSearch from "~/components/forms/custom_inputs/SourceSearch.vue";
-import BuildingSearch from "~/components/forms/custom_inputs/BuildingSearch.vue";
-import JobSearch from "~/components/forms/custom_inputs/JobSearch.vue";
-import PlaceSearch from "~/components/forms/custom_inputs/PlaceSearch.vue";
-import ReligionSearch from "~/components/forms/custom_inputs/ReligionSearch.vue";
-import WeaponSearch from "~/components/forms/custom_inputs/WeaponSearch.vue";
+import EntitySearch from "~/components/forms/custom_inputs/EntitySearch.vue";
 
 const formkitConfig = defaultConfig({
     config: {
         rootClasses,
     },
     inputs: {
-        personAutocomplete: createInput(PersonSearch, { props: ['isMultiple'] }),
-        sourceAutocomplete: createInput(SourceSearch, { props: ['isMultiple'] }),
-        buildingAutocomplete: createInput(BuildingSearch),
-        jobAutocomplete: createInput(JobSearch),
-        placeAutocomplete: createInput(PlaceSearch, { props: ['isMultiple'] }),
-        religionAutocomplete: createInput(ReligionSearch),
-        weaponAutocomplete: createInput(WeaponSearch)
+        entityAutocomplete: createInput(EntitySearch, { props: ['entityType', 'isMultiple', 'optionLabel'] }),
     }
 });
 
