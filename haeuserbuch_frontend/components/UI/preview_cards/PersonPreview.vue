eae0f8996486dae20c7f9b12c92b9569fe0b707b
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-05-22 09:22:37 +0200

    Frontend. Add 'PersonPreview' and 'SourcePreview' components to simplify data representation and navigation for persons and sources (#40)

---
+++ haeuserbuch_frontend/components/UI/preview_cards/PersonPreview.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps<{
+  person: PersonPreviewDTO;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-row items-center gap-2 flex-wrap">
+    <NuxtLink
+        :to="`/persons/${ props.person.id }`"
+        class="rounded-md shadow-sm hover:shadow-md p-1.5 bg-gray-200 font-medium border border-gray-300"
+    >
+      {{ person.fullName }}
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
