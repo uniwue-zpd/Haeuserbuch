74f437f67d6f53afaaa85941b3a5b81cb909c6c6
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-07-01 13:45:55 +0200

    Enhance AppHeader to display user session information and login/logout options (#47)

--- haeuserbuch_frontend/app/components/AppHeader.vue
+++ haeuserbuch_frontend/app/components/AppHeader.vue
@@ -4,6 +4,10 @@ import { ref } from 'vue';
 
 const op = ref();
 const toggle = (event: any) => op.value.toggle(event)
+const { user, loggedIn } = useUserSession();
+const getKeycloak = () => {
+  navigateTo('/auth/keycloak')
+}
 </script>
 
 <template>
@@ -18,8 +22,27 @@ const toggle = (event: any) => op.value.toggle(event)
         <div class="card flex justify-center align-middle text-white">
           <Button type="button" icon="pi pi-user" rounded aria-label="User" variant="link" class="text-white" @click="toggle"/>
           <Popover ref="op">
-            <div class="card flex justify-center">
-              <div>Hier entsteht der Anmeldebereich</div>
+            <div v-if="loggedIn" class="flex flex-col gap-3 p-3">
+              <div class="font-bold montserrat-headline">Willkommen, {{ user?.name || 'Häuserbuch-Mitglied' }}</div>
+              <div>
+                <a
+                    href="/auth/logout"
+                    class="p-1.5 bg-blue-300 border border-gray-400 rounded-md shadow-md font-bold roboto-plain"
+                >
+                  Abmelden
+                </a>
+              </div>
+            </div>
+            <div v-else class="flex flex-col gap-3 p-3">
+              <div class="font-bold montserrat-headline">Sind Sie Projektmitglied?</div>
+              <div>
+                <a
+                    href="/auth/logout"
+                    class="p-1.5 bg-blue-300 border border-gray-400 rounded-md shadow-md font-bold roboto-plain"
+                >
+                  Anmelden
+                </a>
+              </div>
             </div>
           </Popover>
         </div>
