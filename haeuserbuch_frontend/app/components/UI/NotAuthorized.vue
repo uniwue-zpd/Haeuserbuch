490ce0ae4c1394ed48a5fa8e50e5d757128d76c7
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-07-03 13:28:43 +0200

    Frontend. Add 'NotAuthorized' component to hide protected content (#47)

---
+++ haeuserbuch_frontend/app/components/UI/NotAuthorized.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="md:w-1/2 mx-auto mt-10 rounded-xl border border-red-200 bg-white shadow-lg overflow-hidden">
+    <div class="bg-red-50 border-b border-red-200 p-6">
+      <div class="flex flex-col items-center gap-4">
+        <div class="flex h-24 w-24 items-center justify-center rounded-full bg-white border border-red-300 shadow-sm">
+          <Icon
+              name="material-symbols:encrypted-outline-rounded"
+              class="text-black text-6xl"
+          />
+        </div>
+        <p class="montserrat-headline text-xl font-bold text-black">Geschützter Bereich</p>
+      </div>
+    </div>
+    <div class="p-6">
+      <p class="rounded-lg border border-gray-200 bg-gray-50 p-4 text-lg text-center font-mono">
+        Melden Sie sich an, um diesen Bereich zu sehen
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
