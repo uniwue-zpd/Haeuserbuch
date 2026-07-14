d0c32403fcd19f6075d20410a88af2e5d10e9f46
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-07-14 08:44:08 +0200

    Frontend. Update footer layout and styling for improved readability and visual consistency (#53)

--- haeuserbuch_frontend/app/components/AppFooter.vue
+++ haeuserbuch_frontend/app/components/AppFooter.vue
@@ -15,33 +15,43 @@ function copy_to_clipboard(copy_item: string) {
 <template>
   <footer class="flex flex-col">
     <div class="bg-[#2C3E50]">
-      <div class="flex flex-col gap-2 md:flex-row md:justify-between max-w-[1140px] text-black p-3 mx-auto">
-        <div class="flex flex-row justify-between md:space-x-10">
+      <div class="flex flex-col gap-6 md:justify-between max-w-[1140px] text-black p-3 mx-auto">
+        <div class="flex flex-row justify-between space-x-2">
           <div class="flex flex-col gap-2">
-            <p class="text-xl montserrat-headline font-bold text-white">Häuserbuch</p>
-            <NuxtLink to="/project" class="text-white roboto-plain">Projekt</NuxtLink>
-            <NuxtLink to="/faq" class="text-white roboto-plain">FAQ</NuxtLink>
-            <NuxtLink to="/team" class="text-white roboto-plain">Über uns</NuxtLink>
+            <p class="text-lg md:text-xl montserrat-headline font-bold text-white">Häuserbuch</p>
+            <NuxtLink to="/project" class="text-white roboto-plain text-sm md:text-base">Projekt</NuxtLink>
+            <NuxtLink to="/faq" class="text-white roboto-plain text-sm md:text-base">FAQ</NuxtLink>
+            <NuxtLink to="/team" class="text-white roboto-plain text-sm md:text-base">Über uns</NuxtLink>
           </div>
           <div class="flex flex-col gap-2">
-            <p class="text-xl montserrat-headline font-bold text-white">Kontakt</p>
+            <p class="text-lg md:text-xl montserrat-headline font-bold text-white">Kontakt</p>
             <div class="space-x-2">
               <Toast />
-              <a v-bind:href="'mailto:' + email" class="text-white roboto-plain">{{ email }}</a>
-              <i class="pi pi-clone hover:cursor-pointer text-white" @click="copy_to_clipboard(email)"></i>
+              <a v-bind:href="'mailto:' + email" class="text-white roboto-plain text-sm md:text-base">{{ email }}</a>
+              <i class="pi pi-clone hover:cursor-pointer text-white text-sm md:text-base" @click="copy_to_clipboard(email)"></i>
             </div>
-            <NuxtLink to="/contact" class="text-white roboto-plain">Kontaktformular</NuxtLink>
+            <NuxtLink to="/contact" class="text-white roboto-plain text-sm md:text-base">Kontaktformular</NuxtLink>
           </div>
         </div>
-        <div class="flex flex-row justify-between md:space-x-10 md:justify-end">
+        <div class="flex md:flex-row md:justify-between flex-wrap gap-4">
           <div class="flex flex-col justify-center">
             <a href="https://www.dfg.de/de" target="_blank">
-              <img class="h-[30px] md:h-[40px]" src="/dfg_logo_schriftzug_weiss.png" alt="dfglogo"/>
+              <img class="h-[30px] md:h-[55px]" src="/dfg.png" alt="DFG-Logo"/>
             </a>
           </div>
           <div class="flex flex-col justify-center">
             <a href="https://www.uni-wuerzburg.de/" target="_blank">
-              <img class="h-[30px] md:h-[40px]" src="/unilogo4cohne_w.jpg" alt="unilogo"/>
+              <img class="h-[30px] md:h-[55px]" src="/uniwue.jpg" alt="Uni-Wue-Logo"/>
+            </a>
+          </div>
+          <div class="flex flex-col justify-center">
+            <a href="https://www.uni-wuerzburg.de/" target="_blank">
+              <img class="h-[30px] md:h-[55px]" src="/blfd.jpg" alt="BLFD-Logo"/>
+            </a>
+          </div>
+          <div class="flex flex-col justify-center">
+            <a href="https://www.ldbv.bayern.de/" target="_blank">
+              <img class="h-[30px] md:h-[55px]" src="/ldbv.svg" alt="LDBV-Logo"/>
             </a>
           </div>
         </div>
