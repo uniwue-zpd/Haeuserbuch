7e6aa61378af15c761d299bf664e5f0a3425aa28
Tomash Shtohryn <tomashshtohryn@gmail.com>
2026-07-17 15:02:49 +0200

    Frontend. Enhance place details view with improved layout and additional information sections

--- haeuserbuch_frontend/app/pages/places/[id]/index.vue
+++ haeuserbuch_frontend/app/pages/places/[id]/index.vue
@@ -14,7 +14,7 @@ const router = useRoute();
 const placeId = Number(router.params.id);
 const placeStore = usePlaceStore();
 const person_store = usePersonStore();
-const { data: placeItem, error: hasError, pending: isLoading } = await useAsyncData(`place-${ placeId }`, () => placeStore.getPlace(placeId));
+const { data: placeItem, error: hasError, pending: isLoading } = await useAsyncData(`place-${ placeId }-details`, () => placeStore.getPlace(placeId));
 const tile_store = useTileStore();
 const geometry = computed(() => placeItem.value?.geometry as Point | null);
 const properties = computed(() => placeItem.value?.properties as PlaceProperties | null);
@@ -73,51 +73,76 @@ onMounted(async () => {
   <UniversalSkeleton v-if="isLoading"/>
   <FetchError v-else-if="hasError" :error="hasError"/>
   <div v-else>
-    <div class="flex flex-col gap-4 p-4 rounded-md shadow-md border border-gray-200">
+    <div class="flex flex-col gap-4 p-4 rounded-lg shadow-lg border-2 border-gray-300">
       <div class="flex flex-row justify-between">
-        <h1 class="text-3xl montserrat-headline text-black font-bold">{{ properties?.realName }}</h1>
+        <h1 class="text-3xl montserrat-headline font-bold">{{ properties?.realName }}</h1>
         <TaskBar :id="placeId" entity_type="places"/>
       </div>
-      <div class="flex flex-col gap-2">
-        <div id="map" v-show="geometry" class="h-[500px] w-full rounded-md border border-gray-200 shadow-md"/>
-        <div v-if="properties && properties.altNames.length > 0" class="flex flex-col gap-2">
-          <h2 class="text-xl montserrat-headline text-black font-bold">Namensvarianten</h2>
-          <ul class="list-inside list-disc">
-            <li v-for="name in properties?.altNames">
-              {{ name }}
-            </li>
-          </ul>
-        </div>
-        <div v-if="associatedPeople && associatedPeople.length > 0" class="flex flex-col gap-2">
-          <h2 class="text-xl montserrat-headline text-black font-bold">Möglicher Herkunftsort von</h2>
-          <div class="flex flex-wrap gap-3.5">
-            <span v-for="person in associatedPeople" :key="person.id as number">
-              <NuxtLink
-                  :to="`/persons/${ person.id }`"
-                  class="p-1.5 bg-[#F1F2F2] rounded-md shadow-sm hover:shadow-md font-medium roboto-plain"
-              >
-                {{ person.fullName }}
-              </NuxtLink>
-            </span>
-          </div>
+      <div>
+        <div v-if="geometry" class="h-[300px] md:h-[500px] w-full rounded-md shadow-md" id="map"/>
+        <div v-else class="flex flex-col gap-4 items-center justify-center h-[250px] bg-yellow-200 rounded-md mx-auto p-2.5">
+          <i class="pi pi-exclamation-circle text-5xl"/>
+          <p class="roboto-plain text-center text-lg font-medium">Für diesen Ort sind bisher keine Geodaten hinterlegt</p>
         </div>
       </div>
-      <div class="flex flex-col gap-2 p-4 rounded-md shadow-md bg-gray-100 border border-gray-200">
-        <Panel header="Notizen" toggleable v-show="properties?.generalNotes">
-          <template #header>
-            <p class="text-sm text-black roboto-plain font-bold">Notizen</p>
-          </template>
-          <p class="text-sm text-black roboto-plain">{{ properties?.generalNotes }}</p>
-        </Panel>
-        <div class="flex flex-col">
-          <div v-if="properties?.createdDate" class="flex flex-row space-x-2 text-base text-black roboto-plain">
-            <p class="font-bold">Erstellt am:</p>
-            <p>{{ new Date(properties?.createdDate).toLocaleDateString() }}</p>
+      <div v-if="properties" class="flex flex-col gap-4">
+        <div class="roboto-plain flex flex-col gap-5 p-4 rounded-lg shadow-lg border border-gray-300">
+          <h2 class="text-2xl font-semibold montserrat-headline">Informationen zum Ort</h2>
+          <div v-if="properties.altNames" class="flex flex-col gap-2 border-l-4 border-gray-300 pl-3 py-1.5">
+            <p class="text-sm text-gray-500 font-medium">Namensvarianten</p>
+            <ul class="list-inside list-disc">
+              <li
+                  v-for="name in properties.altNames"
+                  class="font-semibold"
+              >
+                {{ name }}
+              </li>
+            </ul>
           </div>
-          <div v-if="properties?.lastModifiedDate" class="flex flex-row space-x-2 text-base text-black roboto-plain">
-            <p class="font-bold">Stand:</p>
-            <p>{{ new Date(properties?.lastModifiedDate).toLocaleDateString() }}</p>
+        </div>
+        <div
+            v-if="properties.generalNotes"
+            class="roboto-plain flex flex-col gap-5 p-4 rounded-lg shadow-lg border border-gray-300"
+        >
+          <h2 class="text-2xl font-semibold montserrat-headline">Notizen und Anmerkungen</h2>
+          <div v-if="properties.generalNotes" class="flex flex-col gap-2 border-l-4 border-gray-300 pl-3 py-1.5">
+            <p class="text-sm text-gray-500 font-medium">Anmerkungen</p>
+            <p class="text-justify">{{ properties.generalNotes }}</p>
           </div>
+          <div v-if="properties.internalNotes" class="flex flex-col gap-2 border-l-4 border-gray-300 pl-3 py-1.5">
+            <p class="text-sm text-gray-500 font-medium">Notizen</p>
+            <p class="text-justify">{{ properties.internalNotes }}</p>
+          </div>
+        </div>
+        <div class="roboto-plain flex flex-col gap-5 p-4 rounded-lg shadow-lg border border-gray-300">
+          <h2 class="text-2xl font-semibold montserrat-headline">Beziehungen zu anderen Entitäten</h2>
+          <div v-if="associatedPeople && associatedPeople.length > 0" class="flex flex-col gap-2 border-l-4 border-gray-300 pl-3 py-1.5">
+            <p class="text-sm text-gray-500 font-medium">Möglicher Herkunftsort von</p>
+            <div class="flex flex-wrap gap-2">
+              <NuxtLink
+                  v-for="person in associatedPeople"
+                  :to="`/persons/${ person.id }`"
+                  class="p-1.5 border-2 border-gray-300 rounded-lg shadow-sm hover:shadow-md font-semibold"
+              >
+                {{ person.fullName || 'Person mit ID' + person.id }}
+              </NuxtLink>
+            </div>
+          </div>
+          <div v-else class="flex flex-col gap-2 border-l-4 border-gray-300 pl-3 py-1.5">
+            <p class="text-sm text-gray-500 font-medium">Bisher keine Relationen gefunden</p>
+          </div>
+        </div>
+        <div class="roboto-plain flex flex-col gap-5 p-4 rounded-lg shadow-lg border border-gray-300">
+          <h2 class="text-2xl font-semibold montserrat-headline">Über den Eintrag</h2>
+          <div v-if="properties.createdDate" class="flex flex-col gap-2 border-l-4 border-gray-300 pl-3 py-1.5">
+            <p class="text-sm text-gray-500 font-medium">Erstellt am</p>
+            <p class="text-justify">{{ new Date(properties.createdDate).toLocaleDateString() }}</p>
+          </div>
+          <div v-if="properties.lastModifiedDate" class="flex flex-col gap-2 border-l-4 border-gray-300 pl-3 py-1.5">
+            <p class="text-sm text-gray-500 font-medium">Zuletzt aktualisiert am</p>
+            <p class="text-justify">{{ new Date(properties.lastModifiedDate).toLocaleDateString() }}</p>
+          </div>
+          <!-- TO-DO: After Keycloak integration: createdBy & lastModifiedBy -->
         </div>
       </div>
     </div>
